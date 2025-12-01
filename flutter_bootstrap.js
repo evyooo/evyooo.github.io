96bfcdcb04ccdbbfe1a4a9c6b85f29505dccb7f6
evyooo <yooooojining@gmail.com>
2025-12-01 11:44:59 +0900

    1201

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4209880214"
+    serviceWorkerVersion: "2162700435"
   }
 });
